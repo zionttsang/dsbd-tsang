88ce9f3234b4b1a127dc85d22ea986c361575ab5
tsang <schwarzen221@gmail.com>
2024-01-25 18:58:39 +0800

    update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import styles from './navbar.module.css'
+import Link from "next/link";
+
 const Navbar = () => {
   return (
-    <div className={styles.container}>Navbar</div>
+    <div className={styles.container}>
+      <div className={styles.social}>LOGO</div>
+      <div className={styles.logo}>zionttsang</div>
+      <div className={styles.menu}>
+        <Link href='/'>Home</Link>
+        <Link href='/'>Contact</Link>
+        <Link href='/'>About</Link>
+      </div>
+    </div>
   )
 }
 
