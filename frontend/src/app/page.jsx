279703211f6dfed5b5f5455fb6fc5afd70787245
Argus <schwarzen221@gmail.com>
2024-01-30 22:26:20 +0800

    post release - work version

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,30 +1,33 @@
-'use client'
+// 'use client'
 
-import Link from "next/link";
-import styles from "./homepage.module.css";
-import { useEffect, useState } from "react";
-import axios from 'axios'
+import Posts from "@/components/posts/Posts";
 
 export default function Home() {
-
-  const [threads, setThreads] = useState([])
-  useEffect(() => {
-    axios.get('http://localhost:3001/getThreads')
-      .then(threads => setThreads(threads.data))
-      .catch(err => console.log(err))
-  }, [])
-
-  return (
-    <tbody>
-      {
-        threads.map(thread => {
-          return <tr>
-            <td><a href={thread.link}>{thread.title}</a></td>
-            <td>{thread.likeCount}</td>
-            <td>{thread.date}</td>
-          </tr>
-        })
-      }
-    </tbody>
-  )
+  return <div>
+    <Posts />
+  </div>
 }
+
+// export default function Home() {
+
+//   const [threads, setThreads] = useState([])
+//   useEffect(() => {
+//     axios.get('http://localhost:3001/getThreads')
+//       .then(threads => setThreads(threads.data))
+//       .catch(err => console.log(err))
+//   }, [])
+
+//   return (
+//     <tbody>
+//       {
+//         threads.map(thread => {
+//           return <tr className={styles.threads}>
+//             <td className={styles.thread}><a href={thread.link}>{thread.title}</a></td>
+//             <td className={styles.likeCount}>{thread.likeCount}</td>
+//             <td className={styles.date}>{thread.date}</td>
+//           </tr>
+//         })
+//       }
+//     </tbody>
+//   )
+// }
--- frontend/src/components/posts/Posts.jsx
+++ frontend/src/components/posts/Posts.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+'use client'
+
+import { useEffect, useState } from 'react'
 import styles from './posts.module.css'
-const Footer = () => {
+import axios from 'axios'
+
+const Posts = () => {
+  const [threads, setThreads] = useState([])
+  useEffect(() => {
+    axios.get('http://localhost:3001/getThreads')
+      .then(threads => setThreads(threads.data))
+      .catch(err => console.log(err))
+  }, [])
+
   return (
-    <div className={styles.container}>
-      <div className={styles.posts}>title</div>
-    </div>
+    <tbody>
+      {
+        threads.map(thread => {
+          return <tr className={styles.threads}>
+            <td className={styles.thread}><a href={thread.link}>{thread.title}</a></td>
+            <td className={styles.likeCount}>{thread.likeCount}</td>
+            <td className={styles.date}>{thread.date}</td>
+          </tr>
+        })
+      }
+    </tbody>
   )
 }
 
-export default Footer
+export default Posts
